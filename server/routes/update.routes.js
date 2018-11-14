a963784747e38df7a74e2aeff5a3ceab21164a07
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-11-14 16:50:39 +0200

    Update route complete, can add comments

---
+++ server/routes/update.routes.js
@@ -0,0 +1,34 @@
+const { Video } = require('../models/Video.model');
+const { uploadReactionFile } = require('../helper/services.helper');
+
+module.exports = [{
+  method: ['PATCH'],
+  path: '/api/video/{id}',
+  config: {
+    payload: {
+      maxBytes: 1000 * 1000 * 50,
+      output: 'stream',
+    },
+  },
+  handler: (request, h) => uploadReactionFile(request.payload.video).then((result, err) => {
+    if (err) {
+      h.response({ statusCode: 400, err });
+    } else {
+      console.log(request.params.id);
+      return Video.findOneAndUpdate({ _id: request.params.id }, {
+        $push: {
+          reactions: {
+            reaction_path: request.payload.video.hapi.filename,
+            'location.type': 'Point',
+            'location.coordinates': [request.payload.longitude, request.payload.latitude],
+            date_created: new Date(),
+          },
+        },
+      }).then((video) => {
+        console.log(video);
+        if (!video) { return h.response({ statusCode: 404 }); }
+        return h.response({ statusCode: 201, video });
+      }).catch(err => h.response({ statusCode: 400, err }));
+    }
+  }),
+}];
