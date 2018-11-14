a8fa1288a2770fa34a0fdbaaef37aff964397361
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-11-14 15:50:39 +0200

    all read methods working

---
+++ server/routes/read.routes.js
@@ -0,0 +1,24 @@
+const { Video } = require('../models/Video.model');
+
+module.exports = [{
+  method: ['GET'],
+  path: '/',
+  handler: (request, reply) => reply.response('Server running'),
+},
+{
+  method: ['GET'],
+  path: '/api/video',
+  handler: (request, reply) => Video.find({}).then((result) => {
+    if (!result) { return reply.response({ statusCode: 404 }); }
+    return result;
+  }).catch(err => reply.response({ statusCode: 400, err })),
+},
+{
+  method: ['GET'],
+  path: '/api/video/{id}',
+  handler: (request, reply) => Video.find({ _id: request.params.id }).then((result) => {
+    if (!result) { return reply.response({ statusCode: 404 }); }
+    return result;
+  }).catch(err => reply.response({ statusCode: 400, err })),
+},
+];
