87bf99c35d18e02d94de0f0271385ab22819e592
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-11-15 19:24:11 +0200

    added static files route

--- server/routes/routes.config.js
+++ server/routes/routes.config.js
@@ -1,5 +1,6 @@
 const createRoutes = require('./create.routes');
 const readRoutes = require('./read.routes');
 const updateRoutes = require('./update.routes');
+const staticRoutes = require('./static.routes');
 
-module.exports = [].concat(createRoutes, readRoutes, updateRoutes);
+module.exports = [].concat(createRoutes, readRoutes, updateRoutes, staticRoutes);
