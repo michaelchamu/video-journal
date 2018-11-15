82956d8da76475e848673b5cf40d0f18677a1f83
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-11-15 19:26:18 +0200

    adjusted API URL from https to http

--- client/src/configs/config.js
+++ client/src/configs/config.js
@@ -1,4 +1,5 @@
-const API_URL = 'https://localhost:7000/api/video';
+const API_URL = 'http://localhost:7000/api/video';
 const mapCenter = [-21.8453859, 20.7264374];
-
-export { API_URL, mapCenter };
+const BASE_VIDEO_URL = 'http://localhost:7000/assets/base/';
+const COMMENTS_VIDEO_URL = 'http://localhost:7000/assets/reactions/';
+export { API_URL, mapCenter, BASE_VIDEO_URL, COMMENTS_VIDEO_URL };
