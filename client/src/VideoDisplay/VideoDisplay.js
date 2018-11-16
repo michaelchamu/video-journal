8028d3d8632ae39b45d668c36c18319ac822a267
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-11-16 21:02:14 +0200

    functional video component

--- client/src/VideoDisplay/VideoDisplay.js
+++ client/src/VideoDisplay/VideoDisplay.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Player } from 'video-react';
+
 import 'video-react/dist/video-react.css';
 import { BASE_VIDEO_URL } from '../configs/config';
 
 const VideoDisplay = props => {
-    const video = BASE_VIDEO_URL + props.video_path;
+    console.log(props);
+    const video = BASE_VIDEO_URL + props.video;
     return (
         <Player playsInline aspectRatio={'auto'} fluid={false} src={video} />
     );
