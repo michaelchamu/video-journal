d279d0c16c38d74a875be8e64195f049e1f49673
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-11-16 20:57:33 +0200

    map boundary

--- client/src/MapDisplay/MapDisplay.js
+++ client/src/MapDisplay/MapDisplay.js
@@ -3,15 +3,15 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import '@ansur/leaflet-pulse-icon/dist/L.Icon.Pulse.js';
 import '@ansur/leaflet-pulse-icon/dist/L.Icon.Pulse.css';
-import { Map, Marker, Popup, Circle } from 'react-leaflet';
+import { Map, Marker, Circle } from 'react-leaflet';
+
 import { mapCenter } from '../configs/config';
-import { VideoDisplay } from '../VideoDisplay/VideoDisplay';
 
 const icon = L.icon.pulse({ iconSize: [10, 10], color: 'red' });
 
 const MapDisplay = props => {
     return (
-        <div>
+        <div className="col-md-9 col-md-offset-2">
             <Map zoomControl={false} setMaxZoom={7} center={mapCenter} zoom={6}>
                 <Circle center={mapCenter} fillColor="red" radius={150000} />
                 <Circle center={mapCenter} fillColor="orange" radius={300000} />
@@ -24,15 +24,10 @@ const MapDisplay = props => {
                                   icon={icon}
                                   key={video._id}
                                   position={video.location.coordinates}
-                              >
-                                  <span>
-                                      <Popup>
-                                          <VideoDisplay
-                                              video_path={video.video_path}
-                                          />
-                                      </Popup>
-                                  </span>
-                              </Marker>
+                                  onClick={() =>
+                                      props.onOpenModal(video.video_path)
+                                  }
+                              />
                           );
                       })
                     : ''}
