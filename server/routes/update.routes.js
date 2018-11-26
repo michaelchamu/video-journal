d1b7e2092770a876dfd94c1769866d2fd0a96955
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-11-27 01:20:05 +0200

    adjust update method to allow creation of comments

--- server/routes/update.routes.js
+++ server/routes/update.routes.js
@@ -1,32 +1,74 @@
+const { ObjectID } = require('mongodb');
 const { Video } = require('../models/Video.model');
-const { uploadReactionFile } = require('../helper/services.helper');
+const { InternationalComments } = require('../models/Comment.model');
+const { uploadReactionFiles, uploadCommentFiles } = require('../helper/services.helper');
 
-module.exports = [{
-  method: ['PATCH'],
-  path: '/api/video/{id}',
-  config: {
-    payload: {
-      maxBytes: 1000 * 1000 * 50,
-      output: 'stream',
+module.exports = [
+  {
+    method: ['PATCH'],
+    path: '/api/video/{snippet}',
+    config: {
+      payload: {
+        maxBytes: 1000 * 1000 * 50,
+        output: 'stream',
+      },
     },
-  },
-  handler: (request, h) => uploadReactionFile(request.payload.video).then((result, err) => {
-    if (err) {
-      h.response({ statusCode: 400, err });
-    } else {
-      return Video.findOneAndUpdate({ _id: request.params.id }, {
-        $push: {
-          reactions: {
-            reaction_path: request.payload.video.hapi.filename,
-            'location.type': 'Point',
-            'location.coordinates': [request.payload.longitude, request.payload.latitude],
-            date_created: new Date(),
+    handler: (request, h) => Video.find({ 'videoSnippet.position': request.params.snippet }).then((snippet) => {
+      if (snippet.length === 0) return h.response({ statusCode: 404 });
+      return uploadReactionFiles(
+        request.payload.video,
+        request.params.snippet,
+        request.payload.reaction,
+      ).then((result, err) => {
+        if (err) return err;
+
+        // else save the reaction to the array of reactions
+        return Video.findByIdAndUpdate(
+          // eslint-disable-next-line no-underscore-dangle
+          { _id: snippet[0]._id },
+          {
+            $push: {
+              reactions: { reactionPath: result.path },
+            },
           },
-        },
-      }).then((video) => {
-        if (!video) { return h.response({ statusCode: 404 }); }
-        return h.response({ statusCode: 201, video });
-      }).catch(err => h.response({ statusCode: 400, err }));
-    }
-  }),
-}];
+        )
+          .then(video => (video.length === 0
+            ? h.response({ statusCode: 404 })
+            : h.response({ statusCode: 200 })))
+          .catch(error => h.response({ statusCode: 404, error }));
+      });
+    }),
+  },
+  {
+    method: ['PATCH'],
+    path: '/api/reaction/{reaction}',
+    config: {
+      payload: {
+        maxBytes: 1000 * 1000 * 50,
+        output: 'stream',
+      },
+    },
+    handler: (request, h) => Video.find({ 'reactions._id': ObjectID(request.params.reaction) }).then((reaction) => {
+      if (reaction.length === 0) return h.response({ statusCode: 404 });
+      return uploadCommentFiles(
+        request.payload.video,
+        request.payload.snippet,
+        request.payload.reaction,
+      ).then((result, error) => {
+        if (error) return h.response({ statusCode: 400, error });
+
+        const comment = new InternationalComments({
+          reactionId: ObjectID(request.params.reaction),
+          country: request.payload.country,
+          commentPath: result.path,
+          dateCreated: new Date(),
+        });
+        return comment
+          .save()
+          .then(addedComment => (comment.length === 0
+            ? h.response({ statusCode: 404, message: 'value not added' })
+            : h.response({ statusCode: 201, addedComment })));
+      });
+    }),
+  },
+];
