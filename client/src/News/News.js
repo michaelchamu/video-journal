941479604f99a58af74990dea78d654bb0060318
Donovan Maasz <dmdmaasz6.reg@outlook.com>
2018-12-01 19:32:30 +0200

    changes to home styles

--- client/src/News/News.js
+++ client/src/News/News.js
@@ -1,47 +1,44 @@
-import React from 'react';
-const news = require('../data/articles.json');
+import React from "react";
+const news = require("../data/articles.json");
 const News = () => {
     return (
-        <div>
-            <div
-                className="col-xs-12"
-                style={{ height: '50%', overflow: 'scroll' }}
-            >
-                <center>
-                    <h1>News Articles</h1>
-                </center>
-                <hr style={{ color: 'black' }} />
-                {news.length > 0
-                    ? news.map((article, index) => {
-                          return (
-                              <div key={index}>
-                                  <div className="row">
-                                      <div className="col-xs-12 col-sm-12 col-md-12">
+        <div
+            className="col-xs-12"
+            style={{ height: "50%", overflow: "scroll" }}
+        >
+            <center>
+                <h1>News Articles</h1>
+            </center>
+            <hr style={{ color: "black" }} />
+            {news.length > 0
+                ? news.map((article, index) => {
+                      return (
+                          <div key={index}>
+                              <div className="row">
+                                  <div className="col-xs-12 col-sm-12 col-md-12">
+                                      <a href={article.uri}>
+                                          <img
+                                              alt={""}
+                                              src={`images/${index + 1}.jpg`}
+                                              className="img-responsive img-box img-thumbnail"
+                                          />
+                                      </a>
+                                  </div>
+                                  <div className="col-xs-12 col-sm-12 col-md-12">
+                                      <h4>
                                           <a href={article.uri}>
-                                              <img
-                                                  alt={''}
-                                                  src={`images/${index +
-                                                      1}.jpg`}
-                                                  className="img-responsive img-box img-thumbnail"
-                                              />
+                                              {article.title}
                                           </a>
-                                      </div>
-                                      <div className="col-xs-12 col-sm-12 col-md-12">
-                                          <h4>
-                                              <a href={article.uri}>
-                                                  {article.title}
-                                              </a>
-                                              -{article.date}
-                                          </h4>
-                                          <p>Placeholder content</p>
-                                          <small>By:{article.author}</small>
-                                      </div>
+                                          -{article.date}
+                                      </h4>
+                                      <p>Placeholder content</p>
+                                      <small>By:{article.author}</small>
                                   </div>
                               </div>
-                          );
-                      })
-                    : null}
-            </div>
+                          </div>
+                      );
+                  })
+                : null}
         </div>
     );
 };
--- client/src/Tweets/Tweets.js
+++ client/src/Tweets/Tweets.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 const Tweets = () => {
     return (
-        <div>
-            <div style={{ height: '50%', overflow: 'scroll' }}>
-                <a
-                    className="twitter-timeline"
-                    href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw"
-                >
-                    Tweets by TwitterDev
-                </a>
-            </div>
+        <div style={{ height: "50%", overflow: "scroll" }}>
+            <a
+                className="twitter-timeline"
+                href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw"
+            >
+                Tweets by TwitterDev
+            </a>
         </div>
     );
 };
