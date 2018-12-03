39bb4d0a3c4dd41811ff09be9e2fca4a35f688f4
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-12-03 22:09:41 +0200

    added comments image and adusted comments component

--- client/src/Comments/Comments.js
+++ client/src/Comments/Comments.js
@@ -1,46 +1,33 @@
-import React from "react";
-import { BASE_VIDEO_URL } from "../configs/config";
-import VideoThumbnail from "react-video-thumbnail";
+import React from 'react';
+import { BASE_VIDEO_URL } from '../configs/config';
+import VideoThumbnail from 'react-video-thumbnail';
 
 const Comments = props => {
     console.log(props);
     return (
         <div>
-            {props.comments.map((comment, index) => {
-                return (
-                    <div
-                        key={index}
-                        className="col-xs-4 col-sm-4 col-lg-4 col-md-4"
-                    >
-                        <a
-                            href="#"
-                            onClick={() =>
-                                props.updateSrc(
-                                    comment.commentPath,
-                                    comment.reactionId
-                                )
-                            }
-                        >
-                            <VideoThumbnail
-                                videoUrl={`${BASE_VIDEO_URL}${
-                                    comment.commentPath
-                                }`}
-                            />
-                        </a>
-                        {/* <img
-                            alt={''}
-                            onClick={() =>
-                                props.updateSrc(
-                                    `${comment.commentPath}`,
-                                    comment.reactionId
-                                )
-                            }
-                            src={`${THUMBNAILS}${comment.thumbnail}`}
-                            className="col-xs-12 col-sm-12"
-                        /> */}
-                    </div>
-                );
-            })}
+            {props.comments
+                ? props.comments.map((comment, index) => {
+                      return (
+                          <div
+                              key={index}
+                              className="col-xs-4 col-sm-4 col-lg-4 col-md-4"
+                          >
+                              <img
+                                  alt={''}
+                                  className="col-md-12 col-xs-12 col-lg-12 col-sm-12"
+                                  src={`images/comment.png`}
+                                  onClick={() =>
+                                      props.updateSrc(
+                                          comment.commentPath,
+                                          comment.reactionId
+                                      )
+                                  }
+                              />
+                          </div>
+                      );
+                  })
+                : 'No comments'}
         </div>
     );
 };
