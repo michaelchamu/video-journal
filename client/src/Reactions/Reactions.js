a147585663af556c6403c3b8cbc033fcbb36250d
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-12-03 22:14:18 +0200

    reactions static image thumbnail

--- client/src/Reactions/Reactions.js
+++ client/src/Reactions/Reactions.js
@@ -1,9 +1,9 @@
-import React from "react";
-import VideoThumbnail from "react-video-thumbnail";
-import { BASE_VIDEO_URL } from "../configs/config";
+import React from 'react';
+import VideoThumbnail from 'react-video-thumbnail';
+import { BASE_VIDEO_URL } from '../configs/config';
 
 const Reactions = props => {
-    console.log(props.videos ? "yes" : null);
+    console.log(props.videos ? 'yes' : null);
     return (
         <div>
             {props.videos.reactions
@@ -13,21 +13,17 @@ const Reactions = props => {
                               key={index}
                               className="col-xs-4 col-sm-4 col-lg-4 col-md-4"
                           >
-                              <a
-                                  href="#"
+                              <img
+                                  alt={'Reaction image'}
+                                  className="col-md-12 col-xs-12 col-lg-12 col-sm-12"
+                                  src={`images/${index + 1}.jpg`}
                                   onClick={() =>
                                       props.updateSrc(
                                           `${reaction.reactionPath}`,
                                           reaction._id
                                       )
                                   }
-                              >
-                                  <VideoThumbnail
-                                      videoUrl={`${BASE_VIDEO_URL}${
-                                          reaction.reactionPath
-                                      }`}
-                                  />
-                              </a>
+                              />
                           </div>
                       );
                   })
