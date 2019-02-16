de810848af90cede3c4b206d8f170ca3c903d448
michaelchamu <chamunorwamicahel@gmail.com>
2019-02-16 18:18:26 +0200

    local env

--- client/src/configs/config.js
+++ client/src/configs/config.js
@@ -1,12 +1,12 @@
-// const API_URL = 'http://localhost:7000/api/';
-// const BASE_VIDEO_URL = 'http://localhost:7000/assets/base/';
-// const COMMENTS_VIDEO_URL = 'http://localhost:7000/assets/reactions/';
-// export { API_URL, BASE_VIDEO_URL, COMMENTS_VIDEO_URL };
+const API_URL = 'http://localhost:7000/api/';
+const BASE_VIDEO_URL = 'http://localhost:7000/assets/base/';
+const COMMENTS_VIDEO_URL = 'http://localhost:7000/assets/reactions/';
+export { API_URL, BASE_VIDEO_URL, COMMENTS_VIDEO_URL };
 
 //https://api.installation.michaelworks.io/
 
-const API_URL = 'https://api.installation.michaelworks.io/api/';
-const BASE_VIDEO_URL = 'https://api.installation.michaelworks.io/assets/base/';
-const COMMENTS_VIDEO_URL =
-    'https://api.installation.michaelworks.io/assets/reactions/';
-export { API_URL, BASE_VIDEO_URL, COMMENTS_VIDEO_URL };
+// const API_URL = 'https://api.installation.michaelworks.io/api/';
+// const BASE_VIDEO_URL = 'https://api.installation.michaelworks.io/assets/base/';
+// const COMMENTS_VIDEO_URL =
+//     'https://api.installation.michaelworks.io/assets/reactions/';
+// export { API_URL, BASE_VIDEO_URL, COMMENTS_VIDEO_URL };
